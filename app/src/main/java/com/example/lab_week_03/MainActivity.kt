eb857012908c7cc717fb32f7847c9864561fdac2
jiversond <141613198+jiversond@users.noreply.github.com>
2025-09-13 21:23:35 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -12,35 +12,20 @@ interface CoffeeListener {
     fun onSelected(id: Int)
 }
 
-class MainActivity : AppCompatActivity(), CoffeeListener {
-
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        // Atur padding supaya tidak ketutupan status bar/navigation bar
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.
+        fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.
+            systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+                systemBars.bottom)
             insets
         }
-
-        // Pertama kali buka, tampilkan ListFragment
-        if (savedInstanceState == null) {
-            val listFragment = ListFragment()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, listFragment)
-                .commit()
-        }
-    }
-
-    // Callback dari ListFragment kalau user pilih kopi
-    override fun onSelected(id: Int) {
-        val detailFragment = DetailFragment.newInstance(id)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, detailFragment)
-            .addToBackStack(null) // biar bisa back ke ListFragment
-            .commit()
     }
 }
+
+
